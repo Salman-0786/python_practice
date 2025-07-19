c43f6865373469964875f7f019545488fed887e9
dev global <skooncode@gmail.com>
2025-07-19 16:55:38 +0530

    Added ArmstrongNumber script

---
+++ Pythonproject/ArmstrongNumber.py
@@ -0,0 +1,14 @@
+def is_armstrong_number(num):
+    num_str = str(num)
+    num_digits = len(num_str)
+    total = sum(int(digit) ** num_digits for digit in num_str)
+    return num == total
+
+try:
+    number = int(input("Enter a number: "))
+    if is_armstrong_number(number):
+        print(f"{number} is an Armstrong number.")
+    else:
+        print(f"{number} is not an Armstrong number.")
+except ValueError:
+    print("Invalid input! Please enter an integer.")
